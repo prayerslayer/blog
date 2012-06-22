af62acde86f2b851c0f3724d1673420a9f5b4395
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-06-23 00:15:46 +0200

    fixed single file compression

--- css/compress.bat
+++ css/compress.bat
@@ -1,1 +1,2 @@
-java -jar yuicompressor.jar --type css --charset UTF-8 bootstrap.css style.css bootstrap-responsive.min.css -o stylesheet.min.css
+copy /b bootstrap.css+style.css+bootstrap-responsive.min.css stylesheet.css
+java -jar yuicompressor.jar --type css --charset UTF-8 stylesheet.css -o stylesheet.min.css
