55fab83fdee7c5c6ab1a89772f9ebab8c7796ef2
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-07-21 09:51:36 +0200

    dateiendung für stylus geändert, sty ist iirc bei latex was

--- compress.sh
+++ compress.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-stylus < css/style.sty > css/style.css
+stylus < css/style.stylus > css/style.css
 cat css/bootstrap.css css/style.css css/bootstrap-responsive.min.css | cleancss -o css/stylesheet.min.css
