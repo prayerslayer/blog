1f9622f175ce13e0e257d5f7ca92008125c8e6f0
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-08-15 15:32:09 +0200

    kicked osm, switched to modestmaps and stamen tiles

--- js/code.js
+++ js/code.js
@@ -1,24 +1,24 @@
-$(document).ready(function() {
-	if ( initOSMap ) //should return true on /cv
-		initOSMap();
+$( document ).ready( function() {
+	if ( initMap ) { //returns true on /cv
+		initMap();
+	}
 
-
-	$(".social")
+	$( ".social" )
 		.tooltip({
 			"placement": "bottom"
 		});
 
 	var host = "http://nikolaus-piccolotto.info";
-	$("img").hover(
+	$( "img" ).hover(
 		function() {
-			$(this).css("opacity", 0.7);
+			$( this ).css("opacity", 0.7);
 		},
 		function() {
-			$(this).css("opacity", 1);
+			$( this ).css("opacity", 1);
 		}
 	);
-	$("header").click(function() {
-		window.location.replace(host);
+	$( "header" ).click(function() {
+		window.location.replace( host );
 	});
 	
 });
