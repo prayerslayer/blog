e3ed5490f22c31146665491292ae23ad73b84acc
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-09-28 20:56:20 +0200

    updated publish script

--- publish.sh
+++ publish.sh
@@ -4,33 +4,36 @@
 date=$(date "+%d.%m.%Y %H:%M")
 
 # go through arguments
-if [[ "$0" == "all" ]]
-then
+if [[ "$0" == "all" ]] then
 	# do everything
 	./compile.sh
 	./compress.sh
+	git add .
+	git commit -m "auto-publish everything on $date"
+	git push uber
+	return 0
+else if [[ "$0" == "posts" ]] then
+		git add _posts/*
+		git commit -m "auto-publish new post on $date"
+		git push uber
+		return 0
+	fi
+else if [[ "$0" == "css" ]] then
+		./compress.sh
+		git add css/*
+		git commit -m "auto-publish new css on $date"
+		git push uber
+		return 0
+	fi
+else 
+	if [[ "$0" == "js" ]] then
+		./compile.sh
+		git add js/*
+		git commit -m "autopublish new js on $date"
+		git push uber
+		return 0
+	fi
 else
-	for arg in "$@"
-	do
-		if [[ arg == "css" ]]
-		then 
-			./compress.sh
-		else 
-			if [[ arg == "js" ]]
-			then
-				./compile.sh
-			fi 
-		fi
-	done
+	ECHO "something went wrong"
+	return -1
 fi
-
-# add everything to git
-git add .
-
-# commit
-git commit -m "auto-publish on $date"
-
-# push
-git push uber
-
-ECHO "publish successful"
