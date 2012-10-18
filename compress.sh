8b19c1836c1d5164906f0c49a9188779cd2a6d66
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-10-18 23:30:59 +0200

    there was a closing bracket missing in simplegrid.css

--- compress.sh
+++ compress.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 stylus < css/style.stylus > css/style.css
-cat  css/normalize.css css/simplegrid.css css/fonts.css css/style.css | cleancss -o css/stylesheet.min.css
+cat  css/normalize.css css/simplegrid.css css/fonts.css css/style.css > css/stylesheet.css
+cleancss -o css/stylesheet.min.css css/stylesheet.css
