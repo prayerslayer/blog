4768f53472dab424eda6256300d2fd9b0288f151
Nikolaus Piccolotto <prayerslayer@gmail.com>
2013-12-20 14:29:22 +0100

    image caption tag

---
+++ _plugins/caption.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  class ImageTag < Liquid::Tag
+    @url = nil
+    @caption = nil
+
+    IMAGE_URL_WITH_CAPTION = /((https?:\/\/|\/)(\S+))(\s+)"(.*?)"/i
+    IMAGE_URL = /((https?:\/\/|\/)(\S+))/i
+
+    def initialize(tag_name, markup, tokens)
+      super
+
+      if markup =~ IMAGE_URL_WITH_CAPTION
+        @url     = $1
+        @caption = $5
+      elsif markup =~ IMAGE_URL
+        @url = $1
+      end
+    end
+
+    def render(context)
+      source  = "<figure>"
+      source += "<figcaption>#{@caption}</figcaption>" if @caption
+      source += "<img src=\"#{@url}\">"
+      source += "</figure>"
+
+      source
+    end
+  end
+end
+
+Liquid::Template.register_tag('image', Jekyll::ImageTag)
