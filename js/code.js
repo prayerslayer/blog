906d1c63d703ba9884c0f3ad28d132a95ac3945b
Nikolaus Piccolotto <prayerslayer@gmail.com>
2014-01-01 20:40:46 +0100

    cross browser code and hopefully minimizing 300ms delays on mobile

--- js/code.js
+++ js/code.js
@@ -1,5 +1,19 @@
-var host = "//npiccolotto.com";
-var header = document.getElementById( "header" );
-header.addEventListener( "click", function() {
-	window.location.href=host;
-}, false );
+var host = "//npiccolotto.com",
+	header = document.getElementById( "header" ),
+	redirect = function() {
+		window.location.href = host;
+	};
+
+if ( header.addEventListener ) {
+	// W3C DOM
+	header.addEventListener( "click", redirect, false );
+}
+else if ( header.attachEvent ) {
+	// IE DOM
+	header.attachEvent( "onclick", redirect );
+}
+else {
+	// No much to do
+	header[ "click" ] = redirect;
+}
+
