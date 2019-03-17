cd6b02f8765c9e5b5cb853af1528b348aab14a31
Nikolaus Piccolotto <prayerslayer@gmail.com>
2019-03-17 01:03:47 +0100

    do not dry-run

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -euox pipefail
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
-synced_files=($(aws s3 sync public s3://npiccolotto.com --dryrun | awk '{ print $5; }' | cut -c21-))
-paths=$(join_by ' ' "${synced_files[@]}")
-aws cloudfront create-invalidation --distribution-id EQPFQ10D8RUNK --paths $paths
+DISTRIBUTION="EQPFQ10D8RUNK"
+
+synced_files=($(aws s3 sync public s3://npiccolotto.com | awk '{ print $1; }' | cut -c21-))
+aws cloudfront create-invalidation --distribution-id "$DISTRIBUTION" --paths $(join_by ' ' "${synced_files[@]}")
